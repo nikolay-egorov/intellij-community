b6387d8f298f5668f738acc576e11ed198b15cc5
nikolay-egorov <deathadder98@gmail.com>
2021-06-02 10:02:48 +0300

    Corrected invocation

--- plugins/stream-debugger/src/com/intellij/debugger/streams/inspect/util/FluxFilteredRequestor.kt
+++ plugins/stream-debugger/src/com/intellij/debugger/streams/inspect/util/FluxFilteredRequestor.kt
@@ -4,24 +4,25 @@ package com.intellij.debugger.streams.inspect.util
 import com.intellij.debugger.DebuggerManager
 import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.engine.DebuggerUtils
+import com.intellij.debugger.engine.evaluation.EvaluateException
+import com.intellij.debugger.engine.evaluation.EvaluateExceptionUtil
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.debugger.engine.events.SuspendContextCommandImpl
+import com.intellij.debugger.impl.ClassLoadingUtils
 import com.intellij.debugger.impl.DebuggerManagerImpl
 import com.intellij.debugger.settings.DebuggerSettings.SUSPEND_THREAD
 import com.intellij.debugger.ui.breakpoints.FilteredRequestorImpl
 import com.intellij.openapi.project.Project
 import com.intellij.xdebugger.XDebugSession
-import com.sun.jdi.Method
-import com.sun.jdi.ObjectReference
-import com.sun.jdi.ThreadReference
-import com.sun.jdi.Value
+import com.sun.jdi.*
 import com.sun.jdi.event.LocatableEvent
 import com.sun.jdi.event.MethodExitEvent
+import org.apache.commons.io.IOUtils
 
 class FluxFilteredRequestor(project: Project, val xSession: XDebugSession) : FilteredRequestorImpl(project) {
   var peekFluxConsumer: Value? = null
   var evaluatedOnce = false
-
+  var wasLoaded = false
   init {
     SUSPEND_POLICY = SUSPEND_THREAD
     CLASS_FILTERS_ENABLED = true
@@ -32,33 +33,86 @@ class FluxFilteredRequestor(project: Project, val xSession: XDebugSession) : Fil
     peekFluxConsumer = value
   }
 
+
+
+  private fun tryLoadClass(evalContextImpl: EvaluationContextImpl, classLoader: ClassLoaderReference,
+                           loadOption: ClassLoaderHelper.Companion.LoadClasses =
+                             ClassLoaderHelper.Companion.LoadClasses.PeekInspector): ReferenceType? {
+    val process = evalContextImpl.debugProcess as DebugProcessImpl
+
+    val loadOptionPair = ClassLoaderHelper.loadOptions[loadOption]
+    val className = loadOptionPair!!.first as String
+    val classPath = loadOptionPair.second as String
+
+    val bytes = IOUtils.toByteArray(
+      this::class.java.classLoader.getResourceAsStream(classPath)
+    )
+
+    if (loadOption != ClassLoaderHelper.Companion.LoadClasses.JavaConsumer) {
+      try {
+        ClassLoadingUtils.defineClass(className, bytes, evalContextImpl, process, classLoader)
+      } catch (e: EvaluateException) {
+        throw EvaluateExceptionUtil.createEvaluateException(e.message, e)
+      }
+    }
+
+    return try {
+      process.loadClass(evalContextImpl, className, classLoader)
+    }
+    catch (e: Exception) {
+      when (e) {
+        is InvocationException, is ClassNotLoadedException, is IncompatibleThreadStateException, is InvalidTypeException -> {
+          throw EvaluateExceptionUtil.createEvaluateException("Could not load class", e)
+        }
+        else -> throw e
+      }
+    }
+  }
+
+
+
+
+
   fun createPeekCall(threadReference: ThreadReference, action: SuspendContextCommandImpl, method: Method, returnValue: Value): Value? {
     val obj = returnValue as ObjectReference
     val peekMethod = DebuggerUtils.findMethod(obj.referenceType(), "doOnNext", null) ?: return null
     //val peekMethod = DebuggerUtils.findMethod(obj.referenceType(), "count", null) ?: return null
 
-    return try {
+    val ans = try {
       val ctx = action.suspendContext ?: return null
-      //val processImpl = ctx.debugProcess
       val manager = DebuggerManager.getInstance(myProject) as DebuggerManagerImpl
       val process = manager.getDebugProcess(xSession.debugProcess.processHandler)
-      val processImpl = process as DebugProcessImpl
+      //val processImpl = process as DebugProcessImpl
+      val processImpl = ctx.debugProcess
 
       var evalCtx = ctx.evaluationContext
       if (evalCtx == null) {
         val frameProxy = ctx.frameProxy!!
         val evlCtx = EvaluationContextImpl(ctx, frameProxy)
         evalCtx = evlCtx
+        //val loader = ClassLoadingUtils.getClassLoader(evalCtx, evalCtx.debugProcess)
+        //evalCtx.debugProcess.suspendManager.voteSuspend(ctx)
+        //if (!wasLoaded) {
+        //  tryLoadClass(evalCtx, loader, ClassLoaderHelper.Companion.LoadClasses.PeekInspectorConsumer)
+        //  wasLoaded = true
+        //}
       }
+      evalCtx.computeAndKeep {
+        peekFluxConsumer
+      }
+      val suspendManager = processImpl.suspendManager
 
-      // works fine with count for some reason
-      processImpl.invokeInstanceMethod(evalCtx, returnValue, peekMethod, listOf(peekFluxConsumer!!), 0)
+      val res = processImpl.invokeInstanceMethod(evalCtx, returnValue, peekMethod, listOf(peekFluxConsumer!!), 0, false)
+      //val res = processImpl.invokeInstanceMethod(evalCtx, returnValue, peekMethod, listOf(), 0, true)
+      res
       // still infinite wait
       //returnValue.invokeMethod(threadReference, peekMethod, listOf(), ObjectReference.INVOKE_SINGLE_THREADED)
     }
     catch (e: Exception) {
       throw e
     }
+
+    return ans
   }
 
   override fun processLocatableEvent(action: SuspendContextCommandImpl, e: LocatableEvent?): Boolean {
@@ -81,9 +135,7 @@ class FluxFilteredRequestor(project: Project, val xSession: XDebugSession) : Fil
     val newReturnVal = createPeekCall(thread, action, method, returnValue) ?: return false
     evaluatedOnce = true
     thread.forceEarlyReturn(newReturnVal)
-    
-    return true
 
-    //return super.processLocatableEvent(action, e)
+    return true
   }
 }
